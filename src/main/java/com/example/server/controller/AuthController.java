61cbec7ac26412aa3a7f1a19fa42616c480135a8
Karolina Pristupa <karolina.pr.8@mail.ru>
2025-11-11 01:52:36 +0300

    feat: add controller (auth) and util (jwt)

---
+++ src/main/java/com/example/server/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.example.server.controller;
+
+import com.example.server.dto.AuthResponseDTO;
+import com.example.server.dto.EnterpriseRegistrationDTO;
+import com.example.server.dto.UserRegistrationDTO;
+import com.example.server.model.Enterprise;
+import com.example.server.model.User;
+import com.example.server.service.EnterpriseService;
+import com.example.server.service.UserService;
+import com.example.server.util.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final EnterpriseService enterpriseService;
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    public AuthController(EnterpriseService enterpriseService, UserService userService, JwtUtil jwtUtil) {
+        this.enterpriseService = enterpriseService;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/enterprise/register")
+    public ResponseEntity<?> registerEnterprise(@RequestBody EnterpriseRegistrationDTO dto) {
+        Enterprise enterprise = enterpriseService.register(dto);
+        return ResponseEntity.ok(enterprise);
+    }
+
+    @PostMapping("/hr/register")
+    public ResponseEntity<?> registerHR(@RequestBody UserRegistrationDTO dto) {
+        User user = userService.registerHR(dto);
+        String token = jwtUtil.generateToken(user.getLogin(), user.getRole().getName());
+
+        AuthResponseDTO response = new AuthResponseDTO();
+        response.setToken(token);
+        response.setUsername(user.getLogin());
+        response.setRole(user.getRole().getName());
+
+        return ResponseEntity.ok(response);
+    }
+
+}
+
---
+++ src/main/java/com/example/server/util/JwtUtil.java
@@ -0,0 +1,57 @@
+package com.example.server.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME;
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .addClaims(Map.of("role", role))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaims(token).getExpiration();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
