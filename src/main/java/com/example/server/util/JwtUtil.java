e2930b12a2c91edcd3a1a15acee5375b20c5c9ce
Karolina Pristupa <karolina.pr.8@mail.ru>
2025-11-11 14:18:52 +0300

    fex: fix auth of HR

--- src/main/java/com/example/server/util/JwtUtil.java
+++ src/main/java/com/example/server/util/JwtUtil.java
@@ -3,20 +3,30 @@ package com.example.server.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 
+
 @Component
 public class JwtUtil {
+
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
     @Value("${jwt.expiration}")
     private long EXPIRATION_TIME;
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(String username, String role) {
         return Jwts.builder()
